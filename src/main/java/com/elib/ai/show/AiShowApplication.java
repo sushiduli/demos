1834d5a047cf301ca66706fed5c59ef7be36868a
lucky-sushi <sushiduli0123@gmail.com>
2020-11-24 20:54:36 +0800

    first commit

---
+++ src/main/java/com/elib/ai/show/AiShowApplication.java
@@ -0,0 +1,13 @@
+package com.elib.ai.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiShowApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/elib/ai/show/controller/BaseController.java
@@ -0,0 +1,46 @@
+package com.elib.ai.show.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @program: ai-show
+ * @description: TODO
+ * @author: konggang
+ * @create: 2020-11-24 19:45
+ **/
+@Controller
+public class BaseController {
+
+    private int i=0;
+
+
+    @GetMapping("/index")
+    public String index(HttpServletRequest request, @RequestParam(value = "name", required = false, defaultValue = "springboot-thymeleaf") String name){
+        request.setAttribute("name", name);
+        return "index";
+    }
+    @GetMapping("/home/index")
+    public String home(HttpServletRequest request, @RequestParam(value = "name", required = false, defaultValue = "springboot-HOME") String name){
+        request.setAttribute("name", name);
+        return "home/home";
+    }
+
+    @GetMapping("/home/show")
+    public String show(HttpServletRequest request, @RequestParam(value = "name", required = false, defaultValue = "springboot-HOME") String name){
+        request.setAttribute("name", name);
+        return "home/show";
+    }
+
+    @PostMapping("/ajax")
+    @ResponseBody
+    public String ajax(HttpServletRequest request){
+        i++;
+        return String.valueOf(i);
+    }
+}
